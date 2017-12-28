095c421c631e744c2afcb773db4a93eecac77568
subhash983 <subhash.983@gmail.com>
2017-12-28 23:57:16 +0530

    updating service

--- scripts/quoteAngular.js
+++ scripts/quoteAngular.js
@@ -2,7 +2,7 @@ import {colors, quotes} from './data.js';
 
 let app = angular.module('quoteMachineApp', []);
 
-app.controller('quoteMachineController', function(quotesData,$http) {
+app.controller('quoteMachineController', function(quotesData) {
     var self = this;
     self.quoteText = "Life isn't about waiting for the storm to pass; it's about learning to dance in the rain.";
     self.author = 'Albert Einstein';
@@ -15,42 +15,25 @@ app.controller('quoteMachineController', function(quotesData,$http) {
     };
     self.getNextQuote = function() {
         //setting background colors
-        $http.get('http://localhost:4000/data').then(function (response) {
-          self.bgColor = {
-              'background-color': response.data.backgroundColor,
-              'color': response.data.backgroundColor
-          };
-          self.iconsbgColor = {
-              'background-color': response.data.backgroundColor
-          };
-          //setting quoteText
-          self.quoteText = response.data.quoteData.quote;
-          self.author = response.data.quoteData.author;
+        quotesData.getQuote().then(function(response) {
+            self.bgColor = {
+                'background-color': response.data.backgroundColor,
+                'color': response.data.backgroundColor
+            };
+            self.iconsbgColor = {
+                'background-color': response.data.backgroundColor
+            };
+            //setting quoteText
+            self.quoteText = response.data.quoteData.quote;
+            self.author = response.data.quoteData.author;
         });
-        // let bgColor = quotesData.getBackgroundColor();
-        // self.bgColor = {
-        //     'background-color': bgColor,
-        //     'color': bgColor
-        // };
-        // self.iconsbgColor = {
-        //     'background-color': bgColor
-        // };
-        // //setting quoteText
-        // let quoteData = quotesData.getQuote();
-        // self.quoteText = quoteData.quote;
-        // self.author = quoteData.author;
     };
 });
 
-app.factory('quotesData', function() {
+app.factory('quotesData', function($http) {
     return {
         getQuote: function() {
-            let quoteIndex = Math.floor(Math.random() * quotes.length);
-            return quotes[quoteIndex];
-        },
-        getBackgroundColor: function() {
-            let colorIndex = Math.floor(Math.random() * colors.length);
-            return colors[colorIndex];
+            return $http.get('http://localhost:4000/data');
         }
     };
 });
